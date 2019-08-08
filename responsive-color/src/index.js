9b4daa93c27f6be6b49fb91d9765113a387f126c
PDmatrix <dimasebakovgoo@gmail.com>
2019-08-08 20:17:36 +0300

    Working responsive color

--- responsive-color/src/index.js
+++ responsive-color/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import config from "../config"
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+window.onload = () => {
+	const calculate = () => {
+		console.log("called")
+		document.body.style.backgroundColor = "white";
+		config.some(([w, h, c]) => {
+			if (w >= window.innerWidth && h >= window.innerHeight) {
+				document.body.style.backgroundColor = c;
+				return true;
+			}
+			return false;
+		})
+	};
+	calculate();
+	window.addEventListener('resize', calculate);
+};
